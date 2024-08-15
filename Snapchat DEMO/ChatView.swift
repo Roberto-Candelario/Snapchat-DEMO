671f7989e36f505b97315047f03aaa1f4a004498
Roberto Candelario <r-jc@live.com>
2024-08-15 19:27:58 -0400

    Added a chat view

---
+++ Snapchat DEMO/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  Snapchat DEMO
+//
+//  Created by Roberto Candelario on 8/15/24.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
