d2cdf09f11527e339232ef1bb57f5d0736c29e72
Roberto Candelario <r-jc@live.com>
2024-08-15 15:02:15 -0400

    Initial Commit

---
+++ Snapchat DEMO/Snapchat_DEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snapchat_DEMOApp.swift
+//  Snapchat DEMO
+//
+//  Created by Roberto Candelario on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Snapchat_DEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Snapchat DEMOTests/Snapchat_DEMOTests.swift
@@ -0,0 +1,17 @@
+//
+//  Snapchat_DEMOTests.swift
+//  Snapchat DEMOTests
+//
+//  Created by Roberto Candelario on 8/15/24.
+//
+
+import Testing
+@testable import Snapchat_DEMO
+
+struct Snapchat_DEMOTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
